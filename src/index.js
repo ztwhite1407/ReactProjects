00071b6937bb0301d6beb103aa48ec4d2d1444be
ztwhite1407 <zachary.white07@gmail.com>
2020-05-14 13:24:32 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CardList from './CardList';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
+
+ReactDOM.render(
+    <CardList robots={robots}/>
+,document.getElementById('root'));
+
+
+serviceWorker.unregister();
